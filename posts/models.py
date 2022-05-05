6423cd26f735b51a38097dca2710ebfd9a2b425b
Ivan Villarrubia <ivan@toximosi.es>
2022-05-05 22:50:38 +0200

    creación del modelo de post

--- posts/models.py
+++ posts/models.py
@@ -4,25 +4,28 @@
 # importaciones ------------------------------------------------------
 #Django
 from django.db import models
+from django.contrib.auth.models import User
+
 
 
 #modelos -------------------------------------------------------------
 class User(models.Model):
-    email = models.EmailField(unique=True)
-    #? unique: para que no se repita el mail en la base de datos
-    password = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    bio = models.TextField(blank=True)#biografia
-    #? blank: acepta espacios vacios
-    birthdate = models.DateField(blank=True, null=True)
-    #?null: permite nulos
-    created = models.DateTimeField(auto_now_add=True)#fecha de creacion
-    #?auto_now_add: que se cree automáticamente
-    modified = models.DateTimeField(auto_now=True)#fecha de modificacion
-
-    is_admin = models.BooleanField(default=False) #filtro, para saber si es administrador
-    #? deafult: valor por defecto
 
     def __str__(self):
         return f"email: {self.email} - password: {self.password} - first_name: {self.first_name} - last_name: {self.last_name} - bio: {self.bio} - birthdate: {self.birthdate} - created: {self.created} - modified: {self.modified} - is_admin: {self.is_admin}"
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #? unique: para que no se repita el mail en la base de datos
+    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
+    #? unique: para que no se repita el mail en la base de datos
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
+
+    created = models.DateTimeField(auto_now_add=True)
+    #?auto_now_add: que se cree automáticamente
+    modified = models.DateTimeField(auto_now=True)
+    #?auto_now: que se cree automáticamente
+
+    def __str__(self):
+        return f"user: {self.user} - profile: {self.profile} - title: {self.title} - photo: {self.photo} - created: {self.created} - modified: {self.modified}"
