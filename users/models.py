03c95d6a8569fa1d626ada30522084bb13b64f09
Ivan Villarrubia <ivan@toximosi.es>
2022-05-08 13:02:21 +0200

    icambiar textos dirmularios User admin

--- users/models.py
+++ users/models.py
@@ -14,16 +14,16 @@ from django.contrib.auth.models import User #heredamos de user de django
 class Profile(models.Model):#en vez de USER le llamamos Profile por perfil de usuario
     """Profile model 
     modelo Proxy que extiende la base de datos con otra información"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Usuario")
     #? on_delete: que hace si se borra el user->en este caso acturá como un SQL
-    biography = models.TextField(blank=True) 
-    phone_number = models.CharField(max_length=20, blank=True)
+    biography = models.TextField(blank=True, verbose_name="Biografía") 
+    phone_number = models.CharField(max_length=20, blank=True, verbose_name="Teléfono")
     #? blank=True, que permite que se quede vacio
-    website = models.URLField(max_length=200, blank=True)
-    picture = models.ImageField(upload_to='users/pictures',blank=True,null=True )#!Necesario instalar pip install Pillowq
+    website = models.URLField(max_length=200, blank=True, verbose_name="Web")
+    picture = models.ImageField(upload_to='users/pictures',blank=True,null=True, verbose_name="Avatar")#!Necesario instalar pip install Pillow
     #upload_to: donde se guardarán las imágenes
-    created = models.DateTimeField(auto_now_add=True)#fechas de creación 
-    modified = models.DateTimeField(auto_now=True)#fecha de modificación 
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creación")#fechas de creación 
+    modified = models.DateTimeField(auto_now=True, verbose_name="Modificación")#fecha de modificación 
     
     def __str__(self): #representacion en string en admin
         return f"user: {self.user} - biography: {self.biography} - website: {self.website} - picture: {self.picture} - created: {self.created} - modified: {self.modified}" 
